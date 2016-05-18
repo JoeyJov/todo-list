0ebf874e7819a670700d6ca034f8868adc96de8e
JoeyJov <jovanovicjoey@gmail.com>
2016-05-18 10:42:38 -0400

    initial

---
+++ js/app.js
@@ -0,0 +1,34 @@
+angular.module("todoApp", [])
+.controller("TodoListController", function($scope) {
+	var todoList = this;
+	todoList.todoItems = [
+		{ name: "Go to school", done: false },
+		{ name: "Eat lunch", done: false },
+		{ name: "Do laundry", done: false },
+		{ name: "Buy materials for project", done: false }
+	]
+
+	todoList.addTodo = function() {
+		todoList.todoItems.push(
+		{ name: todoList.todoText }
+		)
+		todoList.todoText = "";
+	};
+	todoList.remove = function() {
+		var oldTodos = todoList.todoItems;
+		todoList.todoItems = [];
+		angular.forEach(oldTodos, function(todo) {
+			if(!todo.done) todoList.todoItems.push(todo);
+		})
+		todoList.todoItems
+	};
+
+	todoList.remaining = function() {
+		var notCompletedCount = 0;
+		angular.forEach(todoList.todoItems, function(todo) {
+			notCompletedCount += todo.done
+		})
+
+		return notCompletedCount
+	}
+}); // end TodoListController
